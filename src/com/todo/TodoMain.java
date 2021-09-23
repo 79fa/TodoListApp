63b300a94d57c5f454c994519ec2a53f79f08ec4
79fa <hasyhasy2001@gmail.com>
2021-09-23 23:17:29 +0900

    Week3

--- src/com/todo/TodoMain.java
+++ src/com/todo/TodoMain.java
@@ -1,5 +1,6 @@
 package com.todo;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 import com.todo.dao.TodoList;
@@ -8,22 +9,32 @@ import com.todo.service.TodoUtil;
 
 public class TodoMain {
 	
-	public static void start() {
+	public static void start() throws IOException {
 	
 		Scanner sc = new Scanner(System.in);
 		TodoList l = new TodoList();
 		boolean isList = false;
 		boolean quit = false;
+		Menu.displaymenu();
+		
+		
+		TodoUtil.loadList(l,"todolist.txt");
 		do {
-			Menu.displaymenu();
+			
 			isList = false;
+			//System.out.print("Enter your choice >");
+			Menu.prompt();
 			String choice = sc.next();
+			
+			
 			switch (choice) {
 
 			case "add":
 				TodoUtil.createItem(l);
 				break;
-			
+			case "help":
+				Menu.displaymenu();
+				break;
 			case "del":
 				TodoUtil.deleteItem(l);
 				break;
@@ -57,11 +68,15 @@ public class TodoMain {
 				break;
 
 			default:
-				System.out.println("please enter one of the above mentioned command");
+				System.out.println("please enter correct command(need help -help)");
 				break;
 			}
 			
+			
+			
 			if(isList) l.listAll();
 		} while (!quit);
+		
+		TodoUtil.saveList(l,"todolist.txt");
 	}
 }
--- src/com/todo/service/TodoUtil.java
+++ src/com/todo/service/TodoUtil.java
@@ -1,6 +1,8 @@
 package com.todo.service;
 
+import java.io.*;
 import java.util.*;
+import java.util.StringTokenizer;
 
 import com.todo.dao.TodoItem;
 import com.todo.dao.TodoList;
@@ -10,6 +12,7 @@ public class TodoUtil {
 	public static void createItem(TodoList list) {
 		
 		String title, desc;
+		String current_date;
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("\n"
@@ -21,12 +24,13 @@ public class TodoUtil {
 			System.out.printf("title can't be duplicate");
 			return;
 		}
-		
+		sc.nextLine();
 		System.out.println("enter the description");
-		desc = sc.next();
+		desc = sc.nextLine().trim();
 		
 		TodoItem t = new TodoItem(title, desc);
 		list.addItem(t);
+		System.out.println("item added");
 	}
 
 	public static void deleteItem(TodoList l) {
@@ -42,6 +46,7 @@ public class TodoUtil {
 		for (TodoItem item : l.getList()) {
 			if (title.equals(item.getTitle())) {
 				l.deleteItem(item);
+				System.out.println("item delted");
 				break;
 			}
 		}
@@ -68,9 +73,9 @@ public class TodoUtil {
 			System.out.println("title can't be duplicate");
 			return;
 		}
-		
+		sc.nextLine();
 		System.out.println("enter the new description ");
-		String new_description = sc.next().trim();
+		String new_description = sc.nextLine().trim();
 		for (TodoItem item : l.getList()) {
 			if (item.getTitle().equals(title)) {
 				l.deleteItem(item);
@@ -86,5 +91,42 @@ public class TodoUtil {
 		for (TodoItem item : l.getList()) {
 			System.out.println("Item Title: " + item.getTitle() + "  Item Description:  " + item.getDesc());
 		}
+	}
+	public static void loadList(TodoList l,String filename) {
+		//FileReader reader=null;
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("/Users/hasyhasy2001/Desktop/"+filename));
+			String str;
+			
+			while((str = reader.readLine()) != null){
+				String []tokens=str.split("##");
+				TodoItem a = new TodoItem(tokens[0],tokens[1]);
+				a.setCurrent_date(tokens[2]);
+				l.addItem(a);
+	            //System.out.println(str);
+	        }
+			
+			reader.close();
+			
+		}catch(FileNotFoundException e) {
+			System.out.println("파일을 찾을 수 없습니다.");
+		} catch (IOException e) {
+			System.out.println("파일을 읽을 수 없습니다.");
+		}
+		
+		
+	}
+	public static void saveList(TodoList l,String filename) throws IOException {
+		FileWriter writer=new FileWriter("/Users/hasyhasy2001/Desktop/"+filename);
+		
+		for (TodoItem item : l.getList()) {
+			String str=item.toSaveString();
+			System.out.println(str);
+			writer.write(str);
+		}
+		writer.close();
+		
+		
+		
 	}
 }
